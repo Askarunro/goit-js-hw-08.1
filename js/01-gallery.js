64938ad2cd578f27bc45c6307c77bf6a604d1b9e
Askarunro <paskar.y.a@gmail.com>
2021-11-21 10:40:26 +0200

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,6 +1,42 @@
-// Add imports above this line
-import { galleryItems } from "./gallery-items";
+console.log(galleryItems);
+
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
-console.log("Hi");
+const galleryContainar = document.querySelector('.gallery');
+const cardsMarkup = createImgCardsMarkup(galleryItems);
+galleryContainar.insertAdjacentHTML('beforeend', cardsMarkup);
+
+function createImgCardsMarkup(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `
+        <li>
+            <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </li>`;
+    })
+    .join('');
+}
+
+galleryContainar.addEventListener('click', onGalleryLinkClick);
+
+function onGalleryLinkClick(evt) {
+  evt.preventDefault();
+  const isGalleryLinkEl = evt.target.classList.contains('gallery__image');
+  if (!isGalleryLinkEl) {
+    return;
+  }
+  return;
+}
+
+const lightbox = new SimpleLightbox('.gallery__item', {
+  nav: true,
+  captions: true,
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+  infinite: false,
+});
